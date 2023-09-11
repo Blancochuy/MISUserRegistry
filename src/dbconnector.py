d9c1997319ac3af7da0c463a6922e03f36f05751
aauecpr <sebastian.r.hermann@gmail.com>
2023-09-11 17:54:43 -0500

    more stuff

--- src/dbconnector.py
+++ src/dbconnector.py
@@ -1,17 +1,37 @@
+from google.cloud import secretmanager
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 from config.config import spreadsheet_id
 
+import hashlib
+
 class DBConnector():
 
     def __init__(self):
         self.credentials = service_account.Credentials.\
-            from_service_account_file("config/credentials.json", scopes=["https://www.googleapis.com/auth/spreadsheets"])
+            from_service_account_file("src/config/credentials.json", scopes=["https://www.googleapis.com/auth/spreadsheets"])
         self.service = build("sheets", "v4", credentials=self.credentials)
 
         request = self.service.spreadsheets().get(spreadsheetId=spreadsheet_id, ranges=[], includeGridData=False)
         self.sheet_props = request.execute()
+
+    def access_secret_version(self, secret_id, version_id="latest"):
+        # Create the Secret Manager client.
+        client = secretmanager.SecretManagerServiceClient()
+
+        # Build the resource name of the secret version.
+        name = f"projects/MISUserRegistry/secrets/{secret_id}/versions/{version_id}"
+
+        # Access the secret version.
+        response = client.access_secret_version(name=name)
+
+        # Return the decoded payload.
+        return response.payload.data.decode('UTF-8')
+
+    def secret_hash(self, secret_value):
+        # return the sha224 hash of the secret value
+        return hashlib.sha224(bytes(secret_value, "utf-8")).hexdigest()
 
     def get_name(self):
         print(self.sheet_props["properties"]["title"])
--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ if __name__ == "__main__":
     # execute only if run as a script
     MISDB = DBConnector()
     MISDB.get_name()
+    MISDB.secret_hash(MISDB.access_secret_version("config"))
